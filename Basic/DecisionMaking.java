ebc056706ce7ae8840065e552d036aa4d4a75e1e
bhartiiinsa <dukebhartiiprofessional@gmail.com>
2025-06-11 12:54:15 +0530

    decision making

---
+++ Basic/DecisionMaking.java
@@ -0,0 +1,44 @@
+public class DecisionMaking {
+    public static void main(String[] args) {
+        int a,b;
+        a=2;
+        b=5;
+        // 1st way
+
+        // if(b>a)
+        // {
+        //     System.out.println("b is greater");
+
+        // }
+        // else 
+
+        // {
+        //     System.out.println("a is greater");
+        // }
+
+        // 2nd way
+
+        // if(b>a){
+        //     System.out.println("b is greater");
+        // }
+
+        //3rd way 
+        b=1;
+        a=2;
+        if(b>a){
+            System.out.println("b is greater");
+        }
+        else if(b==a)
+        {
+            System.out.println("both are equal");
+        }
+        else{
+            System.out.println("a is greater");
+        }
+
+        
+
+
+    }
+    
+}
