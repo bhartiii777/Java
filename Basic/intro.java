8628474f30f7b8760a3d370fddda50ab940e32b0
Bharti Insan <dukebhaartiiprofessional@gmail.com>
2025-06-11 04:20:38 +0000

    introduction

---
+++ Basic/intro.java
@@ -0,0 +1,12 @@
+public class intro{
+  public static void main(String[] args) {
+    System.out.println("Hello noob");
+    // lets add Two numbers
+    int a,b,c;
+    a=3;
+    b=7;
+    c=a+b;
+    System.out.println(c);
+    
+  }
+}
