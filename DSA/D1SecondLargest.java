71d0c68148a8bec992316664dec1fb3b68520a59
bhartiiinsa <dukebhartiiprofessional@gmail.com>
2025-06-27 13:56:04 +0530

    Second Largest

--- DSA/D1SecondLargest.java
+++ DSA/D1SecondLargest.java
@@ -2,7 +2,9 @@ package DSA;
 
 public class D1SecondLargest {
     public static void main(String[] args) {
-       
+       //finding 2nd largest from given number [1,2,3,4,9,8,7,5,]
+       //first know how to find first largest. i.e 9 here
+      
     }
     
 }
